7f7f2e734e2e2e53703755c3eca804fa6eebf8c3
leptio <91929225+leptio@users.noreply.github.com>
2022-06-13 01:43:24 -0400

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,18 +1,14 @@
-#!/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-brew install wget
 while :
 do
-		wget -q --tries=5 --timeout=1 --spider http://google.com
-		if [[ $? -eq 0 ]]; then
-				echo "Online"
-		else
-				sudo networksetup -setnetworkserviceenabled Wi-Fi off
-				newmac = openssl rand -hex 6 | sed ‘s/(..)/\1:/g; s/.$//’
-				sudo ifconfig en0 ether newmac
-				networksetup -detectnewhardware
-				sudo networksetup -setnetworkserviceenabled Wi-Fi on
-				echo "Offline"
-		fi
+        sleep 5
+        echo -e "GET http://google.com/ HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+                echo "Online"
+        else
+                echo "Offline"
+                sudo networksetup -setnetworkserviceenabled Wi-Fi off && sudo networksetup -setnetworkserviceenabled Wi-Fi on
+                openssl rand -hex 6 | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)/\1:\2:\3:\4:\5:\6/' |  xargs  sudo  ifconfig  en0  ether
+                networksetup -detectnewhardware
+                sleep 10
+        fi
 done
